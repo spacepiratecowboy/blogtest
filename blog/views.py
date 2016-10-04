8baf381542c5720fa5abbddc03e62270cc97eee1
Jasper Bingham <binghamjasper@gmail.com>
2016-10-04 10:40:21 -0400

    added view, fixed up html/css

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
